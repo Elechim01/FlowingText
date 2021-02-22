2aeba68cfc467fe44b3f775c8995cc082534b4d9
Elechim01 <mikymanny015@gmail.com>
2021-02-22 11:26:50 +0100

    Initial Commit

---
+++ FlowingText/FlowingTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowingTextApp.swift
+//  FlowingText
+//
+//  Created by Michele Manniello on 22/02/21.
+//
+
+import SwiftUI
+
+@main
+struct FlowingTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
